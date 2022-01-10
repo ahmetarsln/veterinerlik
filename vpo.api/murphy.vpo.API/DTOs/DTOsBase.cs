572abdc8f37a285d6fdc62815d4ca0387326840f
gulsenagulhas <glsngulhass99@gmail.com>
2022-01-11 01:58:59 +0300

    Project draft created
    Co-Authored-By: Engin Çağlar <36919345+engncaglar13@users.noreply.github.com>
    Co-Authored-By: mhmtyildiz <97394946+mhmtyildiz@users.noreply.github.com>

---
+++ vpo.api/murphy.vpo.API/DTOs/DTOsBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.API.DTOs
+{
+    public class DTOSBase
+    {
+    }
+}
---
+++ vpo.api/murphy.vpo.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.API.Helpers
+{
+    public class AutoMapperProfiles
+    {
+    }
+}
