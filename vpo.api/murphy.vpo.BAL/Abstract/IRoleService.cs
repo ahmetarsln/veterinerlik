c120a2ec7e7864e8d6081de4ce98c77ab774bcf5
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-01-14 12:38:37 +0300

    Create IRoleService.cs

---
+++ vpo.api/murphy.vpo.BAL/Abstract/IRoleService.cs
@@ -0,0 +1,19 @@
+﻿using murphy.vpo.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.BAL.Abstract
+{
+    public interface IRoleService
+    {
+        Task<IList<Role>> GetAllAsync();
+        Task<Role> GetRoleByNameAsync(string rolename);
+        Task AddAsync(Role role);
+        Task UpdateAsync(Role role);
+        Task DeleteAsync(int roleId);
+        Task<Role> GetByIdAsync(int roleId);
+    }
+}
