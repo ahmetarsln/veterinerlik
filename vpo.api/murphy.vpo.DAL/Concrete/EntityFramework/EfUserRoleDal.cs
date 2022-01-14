d5306e582c5c3eac9d26932ff2fb5ea8a8461af2
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-01-14 11:40:03 +0300

    Create EfUserRoleDal.cs

---
+++ vpo.api/murphy.vpo.DAL/Concrete/EntityFramework/EfUserRoleDal.cs
@@ -0,0 +1,20 @@
+﻿using murphy.vpo.Core.DataAccess.EntityFramework;
+using murphy.vpo.DAL.Abstract;
+using murphy.vpo.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.DAL.Concrete.EntityFramework
+{
+    public class EfUserRoleDal : EfEntityRepositoryBase<UserRole>, IUserRoleDal
+    {
+        private VpoDbContext _vpoDbContext;
+        public EfUserRoleDal(VpoDbContext vpoDbContext) : base(vpoDbContext)
+        {
+            _vpoDbContext = vpoDbContext;
+        }
+    }
+}
