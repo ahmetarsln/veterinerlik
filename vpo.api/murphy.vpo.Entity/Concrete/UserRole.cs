5a6d199662fb3a4c23a67d1f636f279bb1442f5f
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-01-14 11:08:37 +0300

    create UserRole entity

---
+++ vpo.api/murphy.vpo.Entity/Concrete/UserRole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.Entity.Concrete
+{
+    class UserRole
+    {
+        public UserRole()
+        { }
+        public int Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
