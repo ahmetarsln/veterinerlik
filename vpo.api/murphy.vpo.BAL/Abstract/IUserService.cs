899901544ec49d1b7f6db51964afb73cad423c4b
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-01-14 11:46:24 +0300

    create IUserService

---
+++ vpo.api/murphy.vpo.BAL/Abstract/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using murphy.vpo.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.BAL.Abstract
+{
+    public interface IUserService
+    {
+        Task<IList<User>> GetAllAsync();
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(int userId);
+        Task<User> GetByIdAsync(int userId);
+    }
+}
