1c43483baedc982cb5ca31ad3a47d15887378a5d
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-01-15 19:01:19 +0300

    Update VpoDbContext.cs

--- vpo.api/murphy.vpo.DAL/Concrete/EntityFramework/VpoDbContext.cs
+++ vpo.api/murphy.vpo.DAL/Concrete/EntityFramework/VpoDbContext.cs
@@ -22,6 +22,7 @@ namespace murphy.vpo.DAL.Concrete.EntityFramework
         public DbSet<Audit> Audits { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Role> Roles { get; set; }
 
         private readonly IHttpContextAccessor _httpContextAccessor;
         public VpoDbContext(DbContextOptions<VpoDbContext> options, IHttpContextAccessor httpContextAccessor) : base(options)
