ecbb82a823e22154b42f9ce0549c36d37b0277f9
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-01-15 17:06:12 +0300

    Create UserRoleDto.cs

---
+++ vpo.api/murphy.vpo.API/DTOs/UserRoleDto.cs
@@ -0,0 +1,21 @@
+﻿using murphy.vpo.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.API.DTOs
+{
+    public class UserRoleDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; }
+        public UserRoleDto()
+        {
+        }
+    }
+}
