6838370008b4bd3d4881092296e8a8c97c6de98c
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-01-15 18:59:13 +0300

    Update AutoMapperProfiles.cs

--- vpo.api/murphy.vpo.API/Helpers/AutoMapperProfiles.cs
+++ vpo.api/murphy.vpo.API/Helpers/AutoMapperProfiles.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using AutoMapper;
+using murphy.vpo.API.DTOs;
+using murphy.vpo.Entity.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace murphy.vpo.API.Helpers
 {
-    public class AutoMapperProfiles
+    public class AutoMapperProfiles : Profile
     {
+        public AutoMapperProfiles() { 
+        CreateMap<User, UserDto>().ReverseMap();
+        CreateMap<Role, RoleDto>().ReverseMap();
+        CreateMap<User, UserRegisterDto>().ReverseMap();
+        CreateMap<User, UserLoginDto>().ReverseMap();
+        CreateMap<User, UserTokenDto>().ReverseMap();
+
+        CreateMap<UserRole, UserRoleDto>().ReverseMap();
+    }
     }
 }
