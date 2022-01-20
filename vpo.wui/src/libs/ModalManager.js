132dedde8ba14b8340e05fb516239bd8c1b120b5
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-01-21 02:14:20 +0300

    ModalManager and SecureRoute created with team
    Co-Authored-By: Engin Çağlar <36919345+engncaglar13@users.noreply.github.com>
    Co-Authored-By: gulsenagulhas <97395132+gulsenagulhas@users.noreply.github.com>

--- vpo.wui/src/libs/ModalManager.js
+++ vpo.wui/src/libs/ModalManager.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import UserNewModal from "../components/Users/UserModals/UserNewModal";
+import UserEditModal from "../components/Users/UserModals/UserEditModal";
+import RoleNewModal from "../components/Roles/RoleModals/RoleNewModal";
+import RoleEditModal from "../components/Roles/RoleModals/RoleEditModal";
+import RoleDeleteModal from "../components/Roles/RoleModals/RoleDeleteModal";
 
 const ModalManager = () => {
   const modalLookup = {
-    
+    UserNewModal,
+    UserEditModal,
+    RoleNewModal,
+    RoleEditModal,
+    RoleDeleteModal,
   };
   const currentModal = useSelector((state) => state.modalReducer);
   let renderedModal;
