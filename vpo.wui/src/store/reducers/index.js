5549ab4a8fb2e891b268eb15d426e1d47a8caa3f
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-01-20 18:40:08 +0300

    user, role, security, modal reducer created with team
    Co-Authored-By: Engin Çağlar <36919345+engncaglar13@users.noreply.github.com>
    Co-Authored-By: gulsenagulhas <97395132+gulsenagulhas@users.noreply.github.com>

--- vpo.wui/src/store/reducers/index.js
+++ vpo.wui/src/store/reducers/index.js
@@ -1,7 +1,13 @@
 import { combineReducers } from "redux";
+import userReducer from "./userReducer";
+import roleReducer from "./roleReducer";
+import securityReducer from "./securityReducer";
+import modalReducer from "./modalReducer";
 
 const rootReducer = combineReducers({
-    
+    userReducer,
+    roleReducer,
+    securityReducer,
     modalReducer,
     
   });
