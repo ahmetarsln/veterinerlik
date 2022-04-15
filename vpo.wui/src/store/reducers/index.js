8aa5b63954f980a7e3da0b9565d8b8a28d14a452
gulsenagulhas <glsngulhass99@gmail.com>
2022-04-15 16:59:31 +0300

    Update index.js

--- vpo.wui/src/store/reducers/index.js
+++ vpo.wui/src/store/reducers/index.js
@@ -3,12 +3,14 @@ import userReducer from "./userReducer";
 import roleReducer from "./roleReducer";
 import securityReducer from "./securityReducer";
 import modalReducer from "./modalReducer";
+import petReducer from "./petReducer";
 
 const rootReducer = combineReducers({
     userReducer,
     roleReducer,
     securityReducer,
     modalReducer,
+    petReducer,
     
   });
   
