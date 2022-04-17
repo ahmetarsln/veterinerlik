64c7c973ebd82a3c2f0785a72249fcb4febc22de
gulsenagulhas <glsngulhass99@gmail.com>
2022-04-17 16:22:52 +0300

    Create ProductDto.cs

---
+++ vpo.api/murphy.vpo.API/DTOs/ProductDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.API.DTOs
+{
+    public class ProductDto
+    {
+        public int id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductPurchasePrice { get; set; }
+        public string ProductSalesPrice { get; set; }
+        public string ProductCategory { get; set; }
+        public string ProductTaxRate { get; set; }
+        public string ProductImage { get; set; }
+    }
+}
