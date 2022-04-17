94114532e8c6906cc95977eaf66ba6b94e143d71
gulsenagulhas <glsngulhass99@gmail.com>
2022-04-17 18:26:47 +0300

    Create 20220416093149_Product added.cs

---
+++ vpo.api/murphy.vpo.API/Migrations/20220416093149_Product added.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace murphy.vpo.API.Migrations
+{
+    public partial class Productadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductPurchasePrice = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductSalesPrice = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductCategory = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductTaxRate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductImage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
