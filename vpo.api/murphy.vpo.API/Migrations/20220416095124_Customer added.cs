85a68e0da60adf3c48bd51e938763dbf2c01f02b
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-04-17 23:16:19 +0300

    added Migrations

---
+++ vpo.api/murphy.vpo.API/Migrations/20220416095124_Customer added.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace murphy.vpo.API.Migrations
+{
+    public partial class Customeradded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CustomerMail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CustomerAdress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CustomerTel = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CustomerNote = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
