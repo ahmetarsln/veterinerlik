0e26f91850f033556b8a00f565ce8509632bbcf2
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-04-21 01:39:40 +0300

    Update index.js

--- vpo.wui/src/store/reducers/index.js
+++ vpo.wui/src/store/reducers/index.js
@@ -3,13 +3,15 @@ import userReducer from "./userReducer";
 import roleReducer from "./roleReducer";
 import securityReducer from "./securityReducer";
 import modalReducer from "./modalReducer";
+import customerReducer from "./customerReducer";
 
 const rootReducer = combineReducers({
     userReducer,
     roleReducer,
     securityReducer,
     modalReducer,
-    
+    customerReducer,
+
   });
   
   export default rootReducer;
