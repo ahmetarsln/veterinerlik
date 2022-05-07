4b84dc0b6ea5083e34f7b7c15deed2acae06a02e
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-05-07 22:05:20 +0300

    Create Supplier.cs

---
+++ vpo.api/murphy.vpo.Entity/Concrete/Supplier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using murphy.vpo.Core.Entities;
+
+namespace murphy.vpo.Entity.Concrete
+{
+    public class Supplier : IEntity, ITrackable, ISoftDeletable
+    {
+        public int Id { get; set; }
+        public string SupplierName { get; set; }
+        public string SupplierTel { get; set; }
+        public string SupplierAuth { get; set; }
+        public string SupplierAdress { get; set; }
+        public string SupplierCurrency { get; set; }
+        public string SupplierMail { get; set; }
+
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
