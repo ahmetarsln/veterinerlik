d31baa32b7512fd78580a9354ee633585cd4085d
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-05-09 17:01:26 +0300

    Create SupplierDto.cs

---
+++ vpo.api/murphy.vpo.API/DTOs/SupplierDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.API.DTOs
+{
+    public class SupplierDto
+    {
+        public int id { get; set; }
+        public string SupplierName { get; set; }
+        public string SupplierTel { get; set; }
+        public string SupplierAuth { get; set; }
+        public string SupplierAdress { get; set; }
+        public string SupplierCurrency { get; set; }
+        public string SupplierMail { get; set; }
+    }
+}
