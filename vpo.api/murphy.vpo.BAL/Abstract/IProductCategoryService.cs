2c4d2ce6f42e1a962754d97a4caa2d15f2b2a943
gulsenagulhas <glsngulhass99@gmail.com>
2022-05-10 01:59:51 +0300

    Create IProductCategoryService.cs

---
+++ vpo.api/murphy.vpo.BAL/Abstract/IProductCategoryService.cs
@@ -0,0 +1,18 @@
+﻿using murphy.vpo.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.BAL.Abstract
+{
+    public interface IProductCategoryService
+    {
+        Task<IList<ProductCategory>> GetAllAsync();
+        Task AddAsync(ProductCategory productCategory);
+        Task UpdateAsync(ProductCategory productCategory);
+        Task DeleteAsync(int productCategoryId);
+        Task<ProductCategory> GetByIdAsync(int productCategoryId);
+    }
+}
