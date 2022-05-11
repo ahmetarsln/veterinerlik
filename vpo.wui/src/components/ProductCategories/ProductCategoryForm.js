fcd83cfed520ec5d16853703e5b37e981f38e45a
gulsenagulhas <glsngulhass99@gmail.com>
2022-05-11 23:11:55 +0300

    Create ProductCategoryForm.js

---
+++ vpo.wui/src/components/ProductCategories/ProductCategoryForm.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Field } from "formik";
+import InputComponent from "../../libs/components/InputComponent";
+const formsection = () => {
+  return (
+    <div>
+      <Field
+        type="text"
+        name="productCategoryName"
+        id="productCategoryName"
+        display="Ürün Kategori Adı"
+        component={InputComponent}
+      />
+      
+      
+      <Button
+        variant="success"
+        as="input"
+        size="lg"
+        type="submit"
+        value="Kaydet"
+      />
+    </div>
+  );
+};
+export default formsection;
