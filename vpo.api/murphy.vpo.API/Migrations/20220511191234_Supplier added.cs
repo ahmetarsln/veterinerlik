8addb9e81af832b4d44027cff10ada77b3b3bd75
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-05-11 22:12:54 +0300

    Migrations added

---
+++ vpo.api/murphy.vpo.API/Migrations/20220511191234_Supplier added.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace murphy.vpo.API.Migrations
+{
+    public partial class Supplieradded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Suppliers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SupplierName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SupplierTel = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SupplierAuth = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SupplierAdress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SupplierCurrency = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SupplierMail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suppliers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Suppliers");
+        }
+    }
+}
