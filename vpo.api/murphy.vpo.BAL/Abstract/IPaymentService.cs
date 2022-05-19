91001251e610d31e57838c190a3720a4c0b50240
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-05-19 20:03:35 +0300

    Create IPaymentService.cs

---
+++ vpo.api/murphy.vpo.BAL/Abstract/IPaymentService.cs
@@ -0,0 +1,18 @@
+﻿using murphy.vpo.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.BAL.Abstract
+{
+    public interface IPaymentService
+    {
+        Task<IList<Payment>> GetAllAsync();
+        Task AddAsync(Payment payment);
+        Task UpdateAsync(Payment payment);
+        Task DeleteAsync(int paymentId);
+        Task<Payment> GetByIdAsync(int paymentId);
+    }
+}
