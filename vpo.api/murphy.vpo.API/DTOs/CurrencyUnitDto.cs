649c2d8e86bbbea81ea5241a3fb4b06d73601d84
gulsenagulhas <glsngulhass99@gmail.com>
2022-05-19 23:41:54 +0300

    Create CurrencyUnitDto.cs

---
+++ vpo.api/murphy.vpo.API/DTOs/CurrencyUnitDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.API.DTOs
+{
+    public class CurrencyUnitDto
+    {
+        public int id { get; set; }
+        public string CurrencyUnitName { get; set; }
+        public string CurrencyUnitCode { get; set; }
+        public string CurrencyUnitRate { get; set; }
+        public string CurrencyUnitActivityStatus { get; set; }
+    }
+}
