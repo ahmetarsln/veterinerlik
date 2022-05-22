89feae346eb09ebe24539123cee240c88897ff8b
gulsenagulhas <glsngulhass99@gmail.com>
2022-05-22 22:16:25 +0300

    Create IMeasurementUnitService.cs

---
+++ vpo.api/murphy.vpo.BAL/Abstract/IMeasurementUnitService.cs
@@ -0,0 +1,18 @@
+﻿using murphy.vpo.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.BAL.Abstract
+{
+    public interface IMeasurementUnitService
+    {
+        Task<IList<MeasurementUnit>> GetAllAsync();
+        Task AddAsync(MeasurementUnit measurementUnit);
+        Task UpdateAsync(MeasurementUnit measurementUnit);
+        Task DeleteAsync(int measurementUnitId);
+        Task<MeasurementUnit> GetByIdAsync(int measurementUnitId);
+    }
+}
