222cf5002e4da335a300792621291f6f7099d80e
gulsenagulhas <glsngulhass99@gmail.com>
2022-05-22 19:13:13 +0300

    Create MeasurementUnit.cs

---
+++ vpo.api/murphy.vpo.Entity/Concrete/MeasurementUnit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using murphy.vpo.Core.Entities;
+
+namespace murphy.vpo.Entity.Concrete
+{
+    public class MeasurementUnit : IEntity, ITrackable, ISoftDeletable
+    {
+        public int Id { get; set; }
+        public string MeasurementUnitName { get; set; }
+        public string UnitOfMeasureValue { get; set; }
+        public string MeasurementUnitDescription { get; set; }
+
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
