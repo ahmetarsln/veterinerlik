6191140ee2183402f610d0d9067cafb8775a2b36
gulsenagulhas <glsngulhass99@gmail.com>
2022-05-31 01:47:00 +0300

    Update VpoDbContext.cs

--- vpo.api/murphy.vpo.DAL/Concrete/EntityFramework/VpoDbContext.cs
+++ vpo.api/murphy.vpo.DAL/Concrete/EntityFramework/VpoDbContext.cs
@@ -29,6 +29,7 @@ namespace murphy.vpo.DAL.Concrete.EntityFramework
         public DbSet<ProductCategory> ProductCategories { get; set; }
         public DbSet<CurrencyUnit> CurrencyUnits { get; set; }
         public DbSet<MeasurementUnit> MeasurementUnits { get; set; }
+        public DbSet<ClinicalInformation> ClinicalInformations { get; set; }
 
         private readonly IHttpContextAccessor _httpContextAccessor;
         public VpoDbContext(DbContextOptions<VpoDbContext> options, IHttpContextAccessor httpContextAccessor) : base(options)
