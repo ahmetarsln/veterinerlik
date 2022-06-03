94afbcee6b37cccda372215d261b40807459eb2d
Engin Çağlar <engncaglar13@gmail.com>
2022-06-03 10:00:36 +0300

    Create TaxRate.cs

---
+++ vpo.api/murphy.vpo.Entity/Concrete/TaxRate.cs
@@ -0,0 +1,20 @@
+﻿using murphy.vpo.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.Entity.Concrete
+{
+    public class TaxRate : IEntity, ITrackable, ISoftDeletable
+    {
+        public string CreatedBy { get ; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public int Id { get; set; }
+        public string TaxName { get; set; }
+        public float Rate { get; set; }
+    }
+}
