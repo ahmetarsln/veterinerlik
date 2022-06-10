276dbf22b6bd660dc447f43b8be0ab5c4ed48255
Engin Çağlar <engncaglar13@gmail.com>
2022-06-10 20:49:00 +0300

    Create ITaxRateService.cs

---
+++ vpo.api/murphy.vpo.BAL/Abstract/ITaxRateService.cs
@@ -0,0 +1,19 @@
+﻿using murphy.vpo.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.BAL.Abstract
+{
+    public interface ITaxRateService
+    {
+
+        Task<IList<TaxRate>> GetAllAsync();
+        Task AddAsync(TaxRate taxRate);
+        Task UpdateAsync(TaxRate taxRate);
+        Task DeleteAsync(int taxRateId);
+        Task<TaxRate> GetByIdAsync(int taxRateId);
+    }
+}
