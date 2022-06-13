4efef89954b88630d638e87337ab5e29bd447d9c
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-06-14 00:29:13 +0300

    Update index.js

--- vpo.wui/src/store/reducers/index.js
+++ vpo.wui/src/store/reducers/index.js
@@ -7,6 +7,7 @@ import customerReducer from "./customerReducer";
 import supplierReducer from "./supplierReducer";
 import paymentReducer from "./paymentReducer";
 import parameterReducer from "./parameterReducer";
+import putativeReducer from "./putativeReducer";
 
 const rootReducer = combineReducers({
   userReducer,
@@ -17,6 +18,7 @@ const rootReducer = combineReducers({
   supplierReducer,
   paymentReducer,
   parameterReducer,
+  putativeReducer
 
 });
 
