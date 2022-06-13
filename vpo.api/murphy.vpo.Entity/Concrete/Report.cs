a9830d56540e822118ebe93337d61eaacd02d622
gulsenagulhas <glsngulhass99@gmail.com>
2022-06-13 03:14:03 +0300

    Create Report.cs

---
+++ vpo.api/murphy.vpo.Entity/Concrete/Report.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using murphy.vpo.Core.Entities;
+
+namespace murphy.vpo.Entity.Concrete
+{
+    public class Report : IEntity, ITrackable, ISoftDeletable
+    {
+        public int Id { get; set; }
+        public int Vaccines { get; set; }
+        public int Payments { get; set; }
+        public int PaymentsComplate { get; set; }
+        public int Treatments { get; set; }
+        public int Collections { get; set; }
+        public int CollectionsComplate { get; set; }
+
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
