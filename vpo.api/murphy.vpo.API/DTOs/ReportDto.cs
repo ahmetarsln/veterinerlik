b58d852deb9df157f3f5213ba35c77cec47212fe
gulsenagulhas <glsngulhass99@gmail.com>
2022-06-13 20:11:17 +0300

    Create ReportDto.cs

---
+++ vpo.api/murphy.vpo.API/DTOs/ReportDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.API.DTOs
+{
+    public class ReportDto 
+    {
+        public int id { get; set; }
+        public int Vaccines { get; set; }
+        public int Payments { get; set; }
+        public int PaymentsComplate { get; set; }
+        public int Treatments { get; set; }
+        public int Collections { get; set; }
+        public int CollectionsComplate { get; set; }
+    }
+}
