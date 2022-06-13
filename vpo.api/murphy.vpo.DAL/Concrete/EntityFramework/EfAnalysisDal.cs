84a137ac7cfde15b7d4eec813eccac5c862de67a
Engin Çağlar <engncaglar13@gmail.com>
2022-06-13 23:00:08 +0300

    Create EfAnalysisDal.cs

---
+++ vpo.api/murphy.vpo.DAL/Concrete/EntityFramework/EfAnalysisDal.cs
@@ -0,0 +1,20 @@
+﻿using murphy.vpo.Core.DataAccess.EntityFramework;
+using murphy.vpo.DAL.Abstract;
+using murphy.vpo.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.DAL.Concrete.EntityFramework
+{
+    public class EfAnalysisDal: EfEntityRepositoryBase<Analysis>,IAnalysisDal
+    {
+        private VpoDbContext _vpoDbContext;
+        public EfAnalysisDal(VpoDbContext vpoDbContext) : base(vpoDbContext)
+        {
+            _vpoDbContext = vpoDbContext;
+        }
+    }
+}
