10562d0621cab5403db147120dafca02a2536874
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-06-14 16:36:59 +0300

    Create ForgetpasswordService.cs

---
+++ vpo.api/murphy.vpo.BAL/Abstract/ForgetpasswordService.cs
@@ -0,0 +1,18 @@
+﻿using murphy.vpo.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.BAL.Abstract
+{
+    public interface IForgetpasswordService
+    {
+        Task<IList<Forgetpassword>> GetAllAsync();
+        Task AddAsync(Forgetpassword forgetpassword);
+        Task UpdateAsync(Forgetpassword forgetpassword);
+        Task DeleteAsync(int forgetpasswordId);
+        Task<Forgetpassword> GetByIdAsync(int forgetpasswordId);
+    }
+}
