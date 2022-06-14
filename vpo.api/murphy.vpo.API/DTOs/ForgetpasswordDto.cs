aed4094ea3a54a0a37ea1ca96744a1eb7fc715c4
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-06-14 18:17:48 +0300

    Create ForgetpasswordDto.cs

---
+++ vpo.api/murphy.vpo.API/DTOs/ForgetpasswordDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.API.DTOs
+{
+    public class ForgetpasswordDto
+    {
+
+        public int id { get; set; }
+        public string Eposta { get; set; }
+    }
+}
