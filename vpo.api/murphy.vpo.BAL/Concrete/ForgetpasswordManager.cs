8d60d635e867766fc1b13107f89afb8b5f509129
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-06-14 16:24:08 +0300

    Create ForgetpasswordManager.cs

---
+++ vpo.api/murphy.vpo.BAL/Concrete/ForgetpasswordManager.cs
@@ -0,0 +1,46 @@
+﻿using murphy.vpo.BAL.Abstract;
+using murphy.vpo.DAL.Abstract;
+using murphy.vpo.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace murphy.vpo.BAL.Concrete
+{
+    public class ForgetpasswordManager : IForgetpasswordService
+    {
+        private readonly IForgetpasswordDal _forgetpasswordDal;
+
+        public ForgetpasswordManager(IForgetpasswordDal forgetpasswordDal)
+        {
+            _forgetpasswordDal = forgetpasswordDal;
+        }
+
+        public async Task AddAsync(Forgetpassword forgetpassword)
+        {
+            await _forgetpasswordDal.AddAsync(forgetpassword, true);
+        }
+
+        public async Task DeleteAsync(int forgetpasswordId)
+        {
+            await _forgetpasswordDal.DeleteAsync(forgetpasswordId, true);
+        }
+
+        public async Task<IList<Forgetpassword>> GetAllAsync()
+        {
+            return await _forgetpasswordDal.GetAllAsync();
+        }
+
+        public async Task<Forgetpassword> GetByIdAsync(int forgetpasswordId)
+        {
+            return await _forgetpasswordDal.GetByIdAsync(forgetpasswordId);
+        }
+
+        public async Task UpdateAsync(Forgetpassword forgetpassword)
+        {
+            await _forgetpasswordDal.UpdateAsync(forgetpassword, forgetpassword.Id, true);
+        }
+    }
+}
