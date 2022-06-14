5fa22275f4cf7b71a38355150ea887e58745ae1f
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-06-14 04:07:10 +0300

    Create Forgetpassword.cs

---
+++ vpo.api/murphy.vpo.Entity/Concrete/Forgetpassword.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using murphy.vpo.Core.Entities;
+
+namespace murphy.vpo.Entity.Concrete
+{
+    public class Forgetpassword : IEntity, ITrackable, ISoftDeletable
+    {
+        public int Id { get; set; }
+        public string Eposta { get; set; }
+
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
