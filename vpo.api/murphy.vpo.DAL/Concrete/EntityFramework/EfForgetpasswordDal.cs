61b94295c6f9d8fb388b5a11afa4cd5e9d7de084
Mehmet Yıldız <97394946+mhmtyildiz@users.noreply.github.com>
2022-06-14 15:41:39 +0300

    Create EfForgetpasswordDal.cs

---
+++ vpo.api/murphy.vpo.DAL/Concrete/EntityFramework/EfForgetpasswordDal.cs
@@ -0,0 +1,21 @@
+﻿using murphy.vpo.Core.DataAccess.EntityFramework;
+using murphy.vpo.DAL.Abstract;
+using murphy.vpo.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace murphy.vpo.DAL.Concrete.EntityFramework
+{
+    public class EfForgetpasswordDal : EfEntityRepositoryBase<Forgetpassword>, IForgetpasswordDal
+    {
+        private VpoDbContext _vpoDbContext;
+        public EfForgetpasswordDal(VpoDbContext tuberDbContext) : base(tuberDbContext)
+        {
+            _vpoDbContext = tuberDbContext;
+        }
+    }
+}
